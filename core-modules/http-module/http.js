a45368d08f965998d5430f7c83bdde2a84a682ca
Tareq1586 <tareqmunawar77@gmail.com>
2023-12-13 22:07:52 +0600

    some update on http- port is changed due to error

--- core-modules/http-module/http.js
+++ core-modules/http-module/http.js
@@ -17,5 +17,6 @@ const server = http.createServer((req, res) => {
 // server.on('connection', () => {
 //   console.log('new connection ...');
 // });
-server.listen(3000);
-console.log('listening on port 3000 ...');
+server.listen(3001);
+
+console.log('listening on port 3001 ...');
