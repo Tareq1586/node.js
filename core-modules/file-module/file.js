a29ed64312f9686369c353a1b220b3871b5eb936
Tareq1586 <tareqmunawar77@gmail.com>
2023-12-15 20:10:05 +0600

    utf-8 explored

--- core-modules/file-module/file.js
+++ core-modules/file-module/file.js
@@ -1,18 +1,19 @@
 // fs module
-// appending to a file
+// append to a file
 // const fs = require('fs');
 
 // fs.writeFileSync('myFile.txt', 'Hello programers');
 // fs.appendFileSync('myFile.txt', ' How are you?');
 
 // reading from a file
-// const fs = require('fs');
+// utf-8 encding converts a buffer into a string
+const fs = require('fs');
 
-// const data = fs.readFileSync('myFile.txt', 'utf-8');
-// console.log(data);
+const data = fs.readFileSync('myFile.txt', 'utf-8');
+console.log(data);
 
 // default behaviour of javascript is asynchrnous way
-// because data comes as a drplet of a stream
+// because data comes as a droplet of a stream
 // reading file in asynchronus way
 // const fs = require('fs');
 
@@ -21,9 +22,9 @@
 // });
 
 // open() method
-const fs = require('fs');
-// create an empty file named mynewfile2.txt:
-fs.open('myFile.txt', 'b', (err, file) => {
-  if (err) throw err;
-  console.log(file);
-});
+// const fs = require('fs');
+// // create an empty file named mynewfile2.txt:
+// fs.open('myFile.txt', 'b', (err, file) => {
+//   if (err) throw err;
+//   console.log(file);
+// });
